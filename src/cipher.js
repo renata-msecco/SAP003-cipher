46cc9b479ecac9fae744622b83fa6e820571107d
renata-msecco <renata.secco@gmail.com>
2019-08-26 16:00:37 -0300

    decifrar letra minuscula

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,47 @@
 window.cipher = {
   encode: encode,
-  decode: decode,
-}
+  decode: decode
+};
 
-function encode(mensagem, deslocamento) {
+function encode(deslocamento, mensagem) {
 //alert(mensagem + offset)
 //return(message + offset)
-  let = encodeMensagem = ""
-  for (let i=0; i <mensagem.length; i++){
-  let formula = ((mensagem.charCodeAt(i) - 65 + deslocamento)%26)+65;
-  encodeMensagem += String.fromCharCode(formula);
+  let encodeMensagem ="";
+  for (let i=0; i <mensagem.length; i++) {
+    if (mensagem.charCodeAt(i)>=65 && mensagem.charCodeAt (i) <=90) {
+      let formula =((mensagem.charCodeAt(i) - 65 + deslocamento)%26)+65;
+      encodeMensagem += String.fromCharCode(formula);
+    }
+    else if (mensagem.charCodeAt(i)>=97 && mensagem.charCodeAt (i)<=122) {
+      let formula =((mensagem.charCodeAt(i) - 97 + deslocamento)%26)+97;
+      encodeMensagem += String.fromCharCode(formula);  
+    }
+    else {
+      let formula = mensagem.charCodeAt(i);
+      encodeMensagem += String.fromCharCode(formula);   
+    }
   }
   return encodeMensagem;
-
 }
 
-function decode(mensagem, deslocamento) {
+function decode(deslocamento, mensagem) {
   //alert(mensagem + offset)
   //return(message + offset)
-   let = decodeMensagem = ""
-    for (let i=0; i <mensagem.length; i++){
-    let formula = ((mensagem.charCodeAt(i) - 90 -deslocamento)%26)+90;
-    decodeMensagem += String.fromCharCode(formula);
+  let decodeMensagem = "";
+  for (let i=0; i <mensagem.length; i++) {
+    if (mensagem.charCodeAt(i)>=65 && mensagem.charCodeAt (i) <=90) {
+      let formula =((mensagem.charCodeAt(i) - 90 - deslocamento)%26)+90;
+      decodeMensagem += String.fromCharCode(formula);
     }
-    return decodeMensagem
+    else if (mensagem.charCodeAt(i)>=97 && mensagem.charCodeAt (i)<=122) {
+      let formula =((mensagem.charCodeAt(i) - 122 - deslocamento)%26)+122;
+      decodeMensagem += String.fromCharCode(formula);  
+    } 
+    else {
+      let formula = mensagem.charCodeAt(i);
+      decodeMensagem += String.fromCharCode(formula);   
+    }
+  }
+  return decodeMensagem;
 }
   
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-function cliquecifra(){
-    event.preventDefault();
-    //colocar event para não dar refresh autommático na pagina, uma vez que trata-se de um botão
-    let mensagem= document.getElementById("message").value;
-    let deslocamento= parseInt (document.getElementById("offset").value);
-    let encodeMensagem = window.cipher.encode(mensagem, deslocamento);
+document.getElementById("btn-encode").addEventListener("click", cliquecifra);
+
+function cliquecifra()
+{
+  event.preventDefault();
+  //colocar event para não dar refresh autommático na pagina, uma vez que trata-se de um botão
+  let mensagem= document.getElementById("message").value;
+  let deslocamento= parseInt (document.getElementById("offset").value);
+  let encodeMensagem = window.cipher.encode(deslocamento, mensagem);
 
   document.getElementById("result").innerHTML = `${encodeMensagem}` ;
 }
 
-function cliquedecifra(){
-event.preventDefault();
+document.getElementById("btn-decode").addEventListener("click", cliquedecifra);
+
+function cliquedecifra()
+{
+  event.preventDefault();
   //colocar event para não dar refresh na pagina, uma vez que tem um botão
   let mensagem= document.getElementById("message").value;
- let deslocamento= parseInt (document.getElementById("offset").value);
- let decodeMensagem = window.cipher.decode(mensagem, deslocamento);
+  let deslocamento= parseInt (document.getElementById("offset").value);
+  let decodeMensagem = window.cipher.decode(deslocamento, mensagem);
 
-document.getElementById("result").innerHTML = `${decodeMensagem}` ;
+  document.getElementById("result").innerHTML = `${decodeMensagem}` ;
+
 }
-
-
-  
+cliquedecifra();
